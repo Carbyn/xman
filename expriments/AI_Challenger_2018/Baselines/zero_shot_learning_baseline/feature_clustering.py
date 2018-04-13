e3a5db6217fb0a974a6449484daf8aab9107fdd6
wuyanhai <wuyanhai@baidu.com>
2018-04-13 16:00:15 +0800

    Use AC for clustering, remove redundant spaces
    Change-Id: I5d867029fe94fb5133b9eba2115e0e91d4400a9a

--- expriments/AI_Challenger_2018/Baselines/zero_shot_learning_baseline/feature_clustering.py
+++ expriments/AI_Challenger_2018/Baselines/zero_shot_learning_baseline/feature_clustering.py
@@ -4,13 +4,30 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from collections import Counter
 import numpy as np
 import pickle
 import json
 import sys
 import os
 
+def load_labels(superclass):
+    labels_path = '%s_test_labels.txt' % (superclass.lower())
+    labels = {}
+    with open(labels_path, 'r') as reader:
+        for row in reader:
+            pair = row.strip().split(' ')
+            labels[pair[0]] = pair[1]
+    return labels
+
+def eval_cluster_result(superclass, images_cluster):
+    labels = load_labels(superclass)
+    eval_result = {}
+    for idx, images in images_cluster.items():
+        #print(idx, len(images))
+        eval_result[idx] = Counter(map(lambda x: labels[x], images)).most_common(3)
+    return eval_result
 
 def main():
     if len(sys.argv) == 2:
@@ -18,6 +35,8 @@ def main():
     else:
         print('Param error')
         exit()
+
+    cluster = 'AC'
 
     classNum = {'Animals': 10, 'Fruits': 10}
 
@@ -35,18 +54,29 @@ def main():
     test_labels_idxes = np.where(np.array(labels_all) == 'test')[0]
     test_features = list(np.array(features_all)[test_labels_idxes])
     test_images = list(np.array(images_all)[test_labels_idxes])
-    #print(len(test_labels_idxes), len(test_features), len(test_images))
 
-    clf = KMeans(n_clusters=classNum[superclass], max_iter=300)
-    s = clf.fit(features_all)
+    if cluster == 'KMeans':
+        clf = KMeans(n_clusters=classNum[superclass], max_iter=300)
+        s = clf.fit(test_features)
+    else:
+        clf = AgglomerativeClustering(n_clusters=classNum[superclass], linkage='complete')
+        cluster_labels = clf.fit_predict(test_features)
+        ac_cluster_centers = {}
+        for i in range(classNum[superclass]):
+            cluster_labels_idxes = np.where(np.array(cluster_labels) == i)[0]
+            sub_test_features = np.array(test_features)[cluster_labels_idxes]
+            ac_cluster_centers[i] = list(np.mean(sub_test_features, axis=0))
 
     idx = 0
     images_cluster = {}
     for image in test_images:
-        cluster_idx = clf.predict([test_features[idx]])
-        cluster_idx = cluster_idx[0]
-        feature_cluster = clf.cluster_centers_[cluster_idx]
-        #print(len(features_all), len(test_labels_idxes), idx)
+        if cluster == 'KMeans':
+            cluster_idx = clf.predict([test_features[idx]])
+            cluster_idx = cluster_idx[0]
+            feature_cluster = clf.cluster_centers_[cluster_idx]
+        else :
+            cluster_idx = cluster_labels[idx]
+            feature_cluster = ac_cluster_centers[cluster_idx]
         features_all[test_labels_idxes[idx]] = feature_cluster
         if str(cluster_idx) not in images_cluster.keys():
             images_cluster[str(cluster_idx)] = []
@@ -56,6 +86,10 @@ def main():
     fimages = open('images_cluster.json', 'w')
     fimages.write(json.dumps(images_cluster))
     fimages.close()
+
+    feval = open('images_cluster_eval.json', 'w')
+    feval.write(json.dumps(eval_cluster_result(superclass, images_cluster)))
+    feval.close()
 
     data_all = {'features_all':features_all, 'labels_all':labels_all,
                 'images_all':images_all}
